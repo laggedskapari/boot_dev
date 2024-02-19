ffa5316eaaa05d93883dae41e024f74af82fc5e5
laggedSkapari <es.rajatyadav@gmail.com>
2024-02-19 22:37:31 +0530

    Exercise

---
+++ object_oriented_programming/unit_dragons.py
@@ -0,0 +1,17 @@
+class Unit:
+    def __init__(self, name, pos_x, pos_y):
+        self.name = name
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def in_area(self, x_1, y_1, x_2, y_2):
+        return ((x_1 <= self.pos_x <= x_2) and (y_1 <= self.pos_y <= y_2))
+
+
+class Dragon(Unit):
+    def __init__(self, name, pos_x, pos_y, fire_range):
+        super().__init__(name, pos_x, pos_y)
+        self.__fire_range = fire_range
+
+    def breathe_fire(self, x, y, units):
+        return [(x - units, y - units), (x + units, y + units)]
