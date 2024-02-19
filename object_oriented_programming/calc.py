3ba32405d601aed216ed6d871dbb62527a2eea1b
laggedSkapari <es.rajatyadav@gmail.com>
2024-02-19 22:36:41 +0530

    Exercise

---
+++ object_oriented_programming/calc.py
@@ -0,0 +1,37 @@
+import Math
+
+
+class Calculator:
+    def __init__(self):
+        self.__result = 0
+
+    def add(self, a):
+        self.__result += a
+
+    def subtract(self, a):
+        self.__result -= a
+
+    def multiply(self, a):
+        self.__result *= a
+
+    def divide(self, a):
+        if a == 0:
+            raise ValueError("can't divide by 0")
+        self.__result /= a
+
+    def modulo(self, a):
+        if a == 0:
+            raise ValueError("can't modulo by 0")
+        self.__result %= a
+
+    def power(self, a):
+        self.__result ** a
+
+    def square_root(self):
+        self.__result = Math.sqrt(self.__result)
+
+    def clear(self):
+        self.__result = 0
+
+    def get_result(self):
+        return self.__result
